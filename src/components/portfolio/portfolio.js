c5f9824093f35cfecda6a720aa4338eed03511ae
Aml Saed <amlsaed220@gmail.com>
2022-12-09 13:25:00 +0200

    'portfilio'

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,9 +1,67 @@
 import React from 'react';
-import './portfolio.css'
-
+import './portfolio.css';
+import IMG1 from '../../assets/1.webp';
+import IMG2 from '../../assets/2.webp';
+import IMG3 from '../../assets/3.webp';
+import IMG4 from '../../assets/4.webp';
+// DO NOT USE THE IMAGES IN PRODUCTION
+const data = [
+  {id: 1,
+  image: IMG1,
+  title: 'Crypto Currency Dashboard & Financial Visualization',
+  github: "https://github.com",
+  demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'},
+  {id: 2,
+  image: IMG2,
+  title: "Charts templates & infographics in Figma",
+  github: 'https://github.com',
+  demo:'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'},
+  {id: 3,
+    image: IMG3,
+    title: "Charts templates & infographics in Figma",
+    github: 'https://github.com',
+    demo:'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'},
+  {id:4,
+    image: IMG4,
+    title: "Charts templates & infographics in Figma",
+    github: 'https://github.com',
+    demo:'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'},
+  {id: 5,
+    image: IMG4,
+    title: "Charts templates & infographics in Figma",
+    github: 'https://github.com',
+    demo:'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'},
+  {id: 6,
+    image: IMG4,
+    title: "Charts templates & infographics in Figma",
+    github: 'https://github.com',
+    demo:'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'}
+  ]
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+        <div className="container portfolio_container">
+        {
+           data.map((data) => {
+                    return (
+                    <article key={data.id} className='portfolio_item'>
+                          <div className="portfolio_item-image">
+                               <img src={data.image} alt={data.title} />
+                          </div>
+                          <h3>{data.title}</h3>
+                          <div className="portfolio_item-cta">
+                                <a href={data.github} className="btn" target='_blank'>Github</a>
+                                <a href={data.demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+                          </div>
+                          
+                    </article>
+                    )})
+            }
+           
+        </div>
+    </section>
   )
 }
 
--- src/components/testmonials/testimonials.js
+++ src/components/testmonials/testimonials.js
@@ -1,8 +1,78 @@
-import React from 'react'
+import React from 'react';
+import './testimonials.css';
 
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+
+
+
+import AVTR1 from '../../assets/1.webp'
+import AVTR2 from '../../assets/2.webp'
+import AVTR3 from '../../assets/3.webp'
+SwiperCore.use([Pagination]);
+const data = [
+  {
+    avatar:AVTR1,
+    name: 'Tina Snow',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porroblanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem doloresdeserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Tina Snow',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porroblanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem doloresdeserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Tina Snow',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porroblanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem doloresdeserunt explicabo nostrum ducimus quasi?'
+  }
+]
 const Testimonials = () => {
   return (
-    <div>testimonials</div>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+          <Swiper 
+          className="container testimonials_container"
+          modules={[ Pagination]}
+          spaceBetween={40}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+         
+          >
+                {
+                  data.map((data,index)=>{
+                    
+                      return(
+                        <SwiperSlide className="testimonial" key={index}>
+                        <div className="client_avatar">
+                              <img src={data.avatar} alt="Avatar One" />
+                        </div>
+                        <h5 className="client_name">{data.name}</h5>
+                        <small className="client_review">
+                                  {data.review}
+                        </small> 
+                    </SwiperSlide>
+                      )
+                  
+                  })}
+                 
+
+                  
+
+                  
+          </Swiper>
+      </section>
   )
 }
 
